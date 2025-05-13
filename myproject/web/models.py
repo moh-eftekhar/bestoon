fbb5c5c4e1fbb7755b5e80de7423b1e20723d127
moh-eftekhar <mohammad.eftekharipour@studenti.polito.it>
2025-05-13 19:26:13 +0200

    modify web.models

--- myproject/web/models.py
+++ myproject/web/models.py
@@ -1,3 +1,14 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Expense(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.BigIntegerField()
+    date = models.DateField()
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.amount}"
